a4fa4991008d743ea92bf6805833716b73f40732
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-02-14 05:26:35 +0530

    UP

---
+++ Print 1 To N Without Loop Solution/Print1ToNWithoutLoop.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    int print(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        print(n-1);
+        cout<<n<<" ";
+    }
+    void printNos(int N)
+    {
+        //Your code here
+        print(N);
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        cin>>N;
+        Solution ob;
+        //calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
