c4632ed56e11a6c4aa0f443559dfd164870472d1
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-02-19 09:02:30 +0530

    up

---
+++ Count Digits Solution/CountDigits.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count=0,rem=0;
+        int s=N;
+        
+        while(s!=0)
+        {
+            rem =s%10;
+            if(rem!=0 && N%rem==0)
+                count++;
+            s=s/10;
+        }
+        return count;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
