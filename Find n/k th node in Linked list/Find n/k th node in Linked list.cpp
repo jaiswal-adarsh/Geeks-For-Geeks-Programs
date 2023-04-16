385a3a5f08c5b5d5ef0b8d6694ac4af525fdb4fa
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-04-16 09:34:16 +0530

    UP

---
+++ Find n/k th node in Linked list/Find n/k th node in Linked list.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    // your code here
+    struct Node* curr=head;
+    int cnt=0;
+    while(curr!=NULL){
+        cnt++;
+        curr=curr->next;
+    }
+    
+    curr=head;
+    int t=cnt%k;
+    if(t==0){
+        t=cnt/k;
+    }
+    else{
+        t=(cnt/k)+1;
+    }
+    
+    while(t!=1){
+        curr=curr->next;
+        t--;
+    }
+    return curr->data;
+
+}
