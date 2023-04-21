5e36dee04d1fb9a032cbf10bb9136db7879a62c4
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-04-21 18:02:24 +0530

    UP

---
+++ Compare two linked lists/Compare two linked lists.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;  
+// Linked list Node structure
+struct Node
+{
+    char c;
+    struct Node *next;
+    
+    Node(char x){
+        c = x;
+        next = NULL;
+    }
+    
+};  
+
+
+int compare(Node *list1, Node *list2); 
+
+
+int main()
+{
+ int t,n;
+ cin>>t;
+ char x;
+ while(t--)
+ {
+ cin>>n;
+ cin>>x;
+  Node *list1 = new Node(x);
+    Node *temp=list1;
+    for(int i=0;i<n-1;i++){
+			cin>>x;
+		temp->next = new Node(x);
+		temp=temp->next;
+		}
+		int m;
+		cin>>m;
+    cin>>x;
+    Node *list2 = new Node(x);
+    temp=list2;
+    for(int i=0;i<m-1;i++){
+			cin>>x;
+		temp->next = new Node(x);
+		temp=temp->next;
+		}
+ 
+    cout << compare(list1, list2)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked list Node structure
+struct Node
+{
+    char c;
+    struct Node *next;
+    
+    Node(char x){
+        c = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+// Compare two strings represented as linked lists
+int compare(Node *list1, Node *list2) 
+{
+     // Code Here
+     Node* curr1=list1;
+     Node* curr2=list2;
+     int flag=0;
+     
+     while(curr1!=NULL && curr2!=NULL){
+         if(curr1->c>curr2->c)
+            return 1;
+         else if(curr1->c<curr2->c)
+            return -1;
+         else{
+            
+         curr1=curr1->next;
+         curr2=curr2->next;
+         }
+     }
+     
+     
+}
