ece922a6d6414611d37205f84b7784abc063f5c3
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-04-29 09:13:20 +0530

    UP

---
+++ Sort a stack/Sort a stack.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+
+void sorted(stack<int> &s, int num){
+    if(s.empty()||(!s.empty()&&s.top()<num)){
+        s.push(num);
+        return;
+    }
+    int x= s.top();
+    s.pop();
+    sorted(s,num);
+    s.push(x);
+   
+}
+
+
+
+void SortedStack :: sort()
+{
+   //Your code here
+   if(s.empty()){
+       return;
+   }
+   int num=s.top();
+   s.pop();
+   sort();
+   sorted(s,num);
+  
+}
