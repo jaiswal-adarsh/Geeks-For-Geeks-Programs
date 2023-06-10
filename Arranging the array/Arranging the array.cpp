f0528c552496a0a24eccabbe88336d10ccc37f9b
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-06-10 12:05:06 +0530

    UP

---
+++ Arranging the array/Arranging the array.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            stack<int>s;
+            int j=0;
+            
+            for(int i=0;i<n;i++){
+                if(arr[i]<0)
+                    arr[j++]=arr[i];
+                else
+                    s.push(arr[i]);
+                
+            }
+            
+            j=n-1;
+            while(!s.empty()){
+                arr[j--]=s.top();
+                s.pop();
+            }
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
