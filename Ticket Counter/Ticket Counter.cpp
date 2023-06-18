d37dcfe92b84f64cf5b41841bd73120372f4755c
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-06-18 21:02:14 +0530

    UP

---
+++ Ticket Counter/Ticket Counter.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        int segments = ceil((float)N/K);
+        if(segments % 2 == 1){
+            return N-segments/2*K;
+        }
+        else{
+            return segments/2*K+1;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
