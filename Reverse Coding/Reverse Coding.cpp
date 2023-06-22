3d05f25e2976f50a9cf438655e898cb62d4985a1
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-06-22 14:39:06 +0530

    UP

---
+++ Reverse Coding/Reverse Coding.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        long long sum=0;
+        int mod=1e9+7;
+        sum=(long long)n*(long long)(n+1);
+        sum/=2;
+        return sum%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
