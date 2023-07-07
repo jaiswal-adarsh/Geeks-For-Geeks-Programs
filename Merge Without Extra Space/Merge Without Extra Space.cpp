f3fa8be6de56025357c31d6ec5a0d2650cbf2116
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-07-07 08:56:24 +0530

    UP

---
+++ Merge Without Extra Space/Merge Without Extra Space.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+            int j=0;
+            vector<int> str;
+            for(int i=0;i<n;i++){
+                str.push_back(arr1[i]);
+            }
+            for(int i=0;i<m;i++){
+                str.push_back(arr2[i]);
+            }
+            sort(str.begin(),str.end());
+            
+            for(int i=0;i<n;i++){
+                arr1[i]=str[i];
+            }
+            for(int i=n;i<str.size();i++){
+                arr2[j++]=str[i];
+            }
+            
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
