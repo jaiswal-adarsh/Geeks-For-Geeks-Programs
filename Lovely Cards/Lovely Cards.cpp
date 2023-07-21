7c79f814a853fcd23fad9c842cd52796328f2450
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-07-21 22:56:48 +0530

    UP

---
+++ Lovely Cards/Lovely Cards.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool lovelyCards(int N, vector<int> &Cards) {
+        // code here
+        int cnt=0;
+        
+        for(int i=0;i<N;i++){
+            if(Cards[i]%2!=0)
+                cnt++;
+        }
+        if(cnt<N && cnt>0)
+        return true;
+        else
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> Cards(N);
+        Array::input(Cards,N);
+        
+        Solution obj;
+        bool res = obj.lovelyCards(N, Cards);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
