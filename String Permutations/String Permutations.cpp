4794b865e99f83e7f7352904d472824c1550a498
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-08-06 03:03:12 +0530

    commit

---
+++ String Permutations/String Permutations.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    private:
+    void solve(string S,vector<string>&ans,int index){
+        if(index>=S.size()){
+            ans.push_back(S);
+            return;
+        }
+        for(int i=index;i<S.size();i++){
+            swap(S[index],S[i]);
+            solve(S,ans,index+1);
+            swap(S[index],S[i]);
+        }
+    }
+    public:
+    //Complete this function
+    
+    vector<string> permutation(string S)
+    {
+        //Your code here
+        vector<string>ans;
+        int index=0;
+        solve(S,ans,index);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string S;
+		cin>>S;
+		Solution ob;
+		vector<string> vec = ob.permutation(S);
+		for(string s : vec){
+		    cout<<s<<" ";
+		}
+		cout<<endl;
+	
+	}
+	return 0;
+}
+// } Driver Code Ends
