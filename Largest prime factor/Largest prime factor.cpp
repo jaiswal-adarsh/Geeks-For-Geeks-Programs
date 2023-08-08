d5ebb02e54c80b8ea4f347d965f7f851670037ac
ADARSH JAISWAL <adarshnayan6@gmail.com>
2023-08-09 01:20:13 +0530

    commit

---
+++ Largest prime factor/Largest prime factor.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here
+        int c=2;
+        int m=0;
+        while(N>1){
+            if(N%c==0){
+                N/=c;
+                m=c;
+            }
+            else{
+                c++;
+            }
+        }
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
