9890954c46677bca72b3b462ec7d88ee3019dd33
Adarsh Jaiswal <adarshnayan6@gmail.com>
2023-11-07 01:16:50 +0530

    UP

---
+++ Sum of upper and lower triangles/Sum of upper and lower triangles.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to return sum of upper and lower triangles of a matrix.
+    vector<int> sumTriangles(const vector<vector<int> >& matrix, int n)
+    {
+        // code here
+        int sumu=0,sumr=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                
+                sumu+=matrix[i][j];
+                sumr+=matrix[j][i];
+            }
+        }
+
+        return{sumu,sumr};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        Solution ob;
+        vector<int> result = ob.sumTriangles(matrix,n);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
