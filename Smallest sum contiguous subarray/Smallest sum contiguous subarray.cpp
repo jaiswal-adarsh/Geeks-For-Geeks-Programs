4e3c8c1927c53a0e567a6754f2edf46bc7c886b2
Adarsh Jaiswal <adarshnayan6@gmail.com>
2023-11-20 16:06:09 +0530

    UP

---
+++ Smallest sum contiguous subarray/Smallest sum contiguous subarray.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      //Code here
+      int ans = INT_MAX;
+      int mini = 0;
+      
+      for(int i = 0;i<a.size();i++){
+          mini += a[i];
+          ans = min(mini,ans);
+          ans = min(ans,a[i]);
+          if(mini > 0){
+              mini = 0;
+          }
+          
+      }
+      return ans;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
