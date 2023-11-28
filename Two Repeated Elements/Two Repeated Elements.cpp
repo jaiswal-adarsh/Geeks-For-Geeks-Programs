6aa0e7b6d5563ed4962d3dfad6f5a64d9bd599f3
Adarsh Jaiswal <adarshnayan6@gmail.com>
2023-11-28 07:59:54 +0530

    UP

---
+++ Two Repeated Elements/Two Repeated Elements.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int N) {
+        // Your code here
+        // unordered_map<int,int>m;
+        int array[N+1]={0};
+        vector<int>v;
+        
+        for(int i=0;i<N+2;i++){
+            if(array[arr[i]]){
+                v.push_back(arr[i]);
+            }
+            else{
+                array[arr[i]]++;
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
