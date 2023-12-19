6d6c632e5d030f097f6eee5d91fab99c51abaefb
Adarsh Jaiswal <adarshnayan6@gmail.com>
2023-12-19 09:02:04 +0530

    UP

---
+++ Rightmost different bit/Rightmost different bit.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here  
+       int t=m^n;
+       if(t==0)return -1;
+        int cnt=1;
+        while(t%2==0 && t>0)
+        {
+            cnt++;
+            t=t>>1;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
