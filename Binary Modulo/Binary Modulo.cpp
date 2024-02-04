3ae980c95b3aa5500ed40fcb33dbef0229316a53
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-02-05 00:02:30 +0530

    UP

---
+++ Binary Modulo/Binary Modulo.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int modulo(string s,int m)
+        {
+            //code here
+            int res = 0;
+            for(char c: s){
+                res=res*2+(c-'0');
+                res=res%m;
+            }
+            return res;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
