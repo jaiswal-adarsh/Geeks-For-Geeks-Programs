fcccf693e5cc2bbea8cd8aefb8acbe51e8e3df2d
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-02-06 07:56:18 +0530

    UP

---
+++ Node at distance/Node at distance.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Node Structure
+/*struct Node
+{
+	int k;
+	Node *left, *right;
+};*/
+
+class Solution
+{
+    public:
+    map<Node*,Node*>parent;
+    vector<Node*>leaf;
+    void solve(Node* root){
+        if(root->left!=NULL){
+            parent[root->left]=root;
+            solve(root->left);
+        }
+        if(root->right!=NULL){
+            parent[root->right]=root;
+            solve(root->right);
+        }
+        if(root->left==NULL&& root->right==NULL){
+            leaf.push_back(root);
+        }
+    }
+    int printKDistantfromLeaf(Node* root, int k)
+    {
+         set<Node*>st;
+         solve(root);
+         for(int i=0;i<leaf.size();i++){
+             int r=k;
+             while(r>0&& parent.find(leaf[i])!=parent.end()){
+                 r--,leaf[i]=parent[leaf[i]];
+             }
+             if(r==0) st.insert(leaf[i]);
+         }
+         return st.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s, ks;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, ks);
+        int k=stoi(ks);
+        Solution obj;
+        cout<<obj.printKDistantfromLeaf(root, k)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
