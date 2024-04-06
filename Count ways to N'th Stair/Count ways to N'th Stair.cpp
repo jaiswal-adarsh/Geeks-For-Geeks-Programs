01927bf03ec1e66c63c4e6a88b8274a146862bc1
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-04-06 10:39:17 +0530

    UP

---
+++ Count ways to N'th Stair/Count ways to N'th Stair.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    long long countWays(int n) {
+        // your code here
+        vector<int>v(n,1);
+        long long cnt=0;
+        while(v.size()>1){
+             v.pop_back();
+                v.pop_back();
+                cnt++;
+        }
+
+        return cnt+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    // taking count of testcases
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // taking stair count
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.countWays(n) << endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+// } Driver Code Ends
