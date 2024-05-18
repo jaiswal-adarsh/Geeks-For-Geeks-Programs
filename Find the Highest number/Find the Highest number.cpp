5f50d006a6ec30e7452955cea5ffc0d5a37a91e3
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-05-18 22:13:04 +0530

    UP

---
+++ Find the Highest number/Find the Highest number.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        int n=a.size();
+        for(int i=1;i<n;i++){
+            if(a[i]<a[i-1])
+                return a[i-1];
+        }
+        return a[n-1];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
