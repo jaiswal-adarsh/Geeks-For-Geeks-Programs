f806c495380a9560f220ebb019410464b510a2aa
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-05-25 23:55:35 +0530

    UP

---
+++ You and your book/You and your books.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+        long long ans=0,mxm=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k){
+                mxm +=arr[i];
+            }
+            if(i==n-1 || arr[i+1]>k){
+                ans = max(ans,mxm);
+                mxm=0;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
