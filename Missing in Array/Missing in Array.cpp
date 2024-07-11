0538792ec1f73003362ba6391d8af89ed068c71c
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-07-11 21:08:15 +0530

    UP

---
+++ Missing in Array/Missing in Array.cpp
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    
+    # Note that the size of the array is n-1
+    def missingNumber(self, n, arr):
+        
+        # code here
+        a_sum = sum(arr)
+        e_sum=(n*(n+1))//2
+        return e_sum - a_sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int(input())
+for _ in range(0, t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    s = Solution().missingNumber(n, arr)
+    print(s)
+
+# } Driver Code Ends
