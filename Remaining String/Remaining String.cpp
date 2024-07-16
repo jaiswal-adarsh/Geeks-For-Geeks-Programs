7c5dbebef54d97023a66c253bbf6fc34953a5728
Adarsh Jaiswal <adarshnayan6@gmail.com>
2024-07-16 22:26:40 +0530

    UP

---
+++ Remaining String/Remaining String.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(count==0){
+                ans=ans+s[i];
+            }
+            else if(count>0 && s[i]==ch){
+                count--;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
